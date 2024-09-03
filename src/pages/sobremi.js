06d0ba03d429376e6af930f241446c66373b2ece
albrtovicente <albervifetandem@gmail.com>
2024-09-03 12:18:02 +0200

    blog

---
+++ src/pages/sobremi.js
@@ -0,0 +1,14 @@
+import '../pages/index.css'
+import * as React from "react"
+import { StaticImage } from "gatsby-plugin-image"
+
+const Sobremi = () => {
+  return(
+    <>
+    
+    
+    </>
+  )
+}
+export default Sobremi
+
